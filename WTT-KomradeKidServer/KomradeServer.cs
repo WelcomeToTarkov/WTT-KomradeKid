98632e435b5afc50dea252c20223dbec1d4f5a41
GrooveypenguinX <jakechase2110@gmail.comX>
2025-12-04 16:44:00 -0500

    version bump + csproj fixes

--- WTT-KomradeKidServer/KomradeServer.cs
+++ WTT-KomradeKidServer/KomradeServer.cs
@@ -15,10 +15,13 @@ public record ModMetadata : AbstractModMetadata
     public override string Name { get; init; } = "WTT-KomradeKidServer";
     public override string Author { get; init; } = "GrooveypenguinX";
     public override List<string>? Contributors { get; init; } = null;
-    public override SemanticVersioning.Version Version { get; init; } = new("1.0.0");
+    public override SemanticVersioning.Version Version { get; init; } = new(typeof(ModMetadata).Assembly.GetName().Version?.ToString(3));
     public override Range SptVersion { get; init; } = new("~4.0.2");
     public override List<string>? Incompatibilities { get; init; }
-    public override Dictionary<string, Range>? ModDependencies { get; init; }
+    public override Dictionary<string, Range>? ModDependencies { get; init; } = new()
+    {
+        { "com.wtt.commonlib", new Range("~2.0.6") }
+    };
     public override string? Url { get; init; }
     public override bool? IsBundleMod { get; init; } = true;
     public override string License { get; init; } = "MIT";
